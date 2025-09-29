459a5d4cd92edb27318995837af35eb87404a30a
Miranda <lucasmiranda.pro@gmail.com>
2025-09-29 17:08:34 +0200

    livraison avant ID

---
+++ Gemfile
@@ -0,0 +1,10 @@
+source 'https://rubygems.org'
+ruby '3.4.2'
+gem 'pry'
+gem 'puma'
+gem 'rackup', '~> 2.2'
+gem 'rerun', '~> 0.14.0'
+gem 'rubocop'
+gem 'sinatra', '~> 4.1', '>= 4.1.1'
+
+gem "csv", "~> 3.3"
---
+++ lib/gossip.rb
@@ -0,0 +1,24 @@
+require 'csv'
+
+class Gossip
+  attr_accessor :author, :content, :id
+
+  def initialize(gossip_author, gossip_content)
+    @author = gossip_author
+    @content = gossip_content
+  end
+
+  def save
+    CSV.open('./db/gossip.csv', 'ab') do |csv|
+      csv << [@author, @content]
+    end
+  end
+
+  def self.all
+    all_gossips = []
+    CSV.read('./db/gossip.csv').each do |csv_line|
+      all_gossips << Gossip.new(csv_line[0], csv_line[1])
+    end
+    all_gossips
+  end
+end
