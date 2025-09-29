48f80edc0ecfa2f6bfeacf6f63a2d0085d437dda
Miranda <lucasmiranda.pro@gmail.com>
2025-09-29 20:17:27 +0200

    avant CSS

--- lib/controller.rb
+++ lib/controller.rb
@@ -9,6 +9,19 @@ class ApplicationController < Sinatra::Base
     erb :new_gossip
   end
 
+  get '/gossips/:id/' do
+    id_str = params[:id]
+    idx = id_str.to_i - 1
+    list = Gossip.all
+
+    if idx < 0 || idx >= list.size
+      'Potin introuvable'
+    else
+      g = list[idx]
+      erb :show, locals: { gossip: g }
+    end
+  end
+
   post '/gossips/new/' do
     Gossip.new(params['gossip_author'], params['gossip_content']).save
     redirect '/'
